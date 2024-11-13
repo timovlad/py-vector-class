bbfc742a8c1925af88fa50ac7350f5a8f76d2747
timovlad <61474934+timovlad@users.noreply.github.com>
2024-11-13 12:15:29 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -26,9 +26,10 @@ class Vector:
             raise ValueError("Unsupported type for multiplication")
 
     @classmethod
-    def create_vector_by_two_points
-    (cls, start_point: Tuple[float, float],
-     end_point: Tuple[float, float]) -> "Vector":
+    def create_vector_by_two_points(
+        cls, start_point: Tuple[float, float],
+     end_point: Tuple[float, float]
+    ) -> "Vector":
         return cls(end_point[0] - start_point[0], end_point[1] - start_point[1])
 
     def get_length(self) -> float:
