b4ad2e7cffef87fb0d7a811eac6c95d122799f13
timovlad <61474934+timovlad@users.noreply.github.com>
2024-11-13 13:01:00 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,25 +3,21 @@ from typing import Tuple
 
 
 class Vector:
-    def __init__(self, x_coordinate: float, y_coordinate: float) -> None:
-        self.x_coordinate = round(x_coordinate, 2)
-        self.y_coordinate = round(y_coordinate, 2)
+    def __init__(self, x: float, y: float) -> None:
+        self.x = round(x, 2)
+        self.y = round(y, 2)
 
     def __add__(self, other: "Vector") -> "Vector":
-        return Vector(self.x_coordinate + other.x_coordinate,
-                      self.y_coordinate + other.y_coordinate)
+        return Vector(self.x + other.x, self.y + other.y)
 
     def __sub__(self, other: "Vector") -> "Vector":
-        return Vector(self.x_coordinate - other.x_coordinate,
-                      self.y_coordinate - other.y_coordinate)
+        return Vector(self.x - other.x, self.y - other.y)
 
     def __mul__(self, other: any) -> any:
         if isinstance(other, (int, float)):
-            return Vector(self.x_coordinate * other,
-                          self.y_coordinate * other)
+            return Vector(self.x * other, self.y * other)
         elif isinstance(other, Vector):
-            return round(self.x_coordinate * other.x_coordinate
-                         + self.y_coordinate * other.y_coordinate, 4)
+            return round(self.x * other.x + self.y * other.y, 4)
         else:
             raise ValueError("Unsupported type for multiplication")
 
@@ -36,13 +32,13 @@ class Vector:
         )
 
     def get_length(self) -> float:
-        return math.sqrt(self.x_coordinate ** 2 + self.y_coordinate ** 2)
+        return math.sqrt(self.x ** 2 + self.y ** 2)
 
     def get_normalized(self) -> "Vector":
         length = self.get_length()
         if length == 0:
             return Vector(0, 0)
-        return Vector(self.x_coordinate / length, self.y_coordinate / length)
+        return Vector(self.x / length, self.y / length)
 
     def angle_between(self, vector: "Vector") -> int:
         dot_product = self * vector
@@ -59,8 +55,8 @@ class Vector:
 
     def rotate(self, degrees: int) -> "Vector":
         radians = math.radians(degrees)
-        new_x = (self.x_coordinate * math.cos(radians)
-                 - self.y_coordinate * math.sin(radians))
-        new_y = (self.x_coordinate * math.sin(radians)
-                 + self.y_coordinate * math.cos(radians))
+        new_x = (self.x * math.cos(radians)
+                 - self.y * math.sin(radians))
+        new_y = (self.x * math.sin(radians)
+                 + self.y * math.cos(radians))
         return Vector(new_x, new_y)
