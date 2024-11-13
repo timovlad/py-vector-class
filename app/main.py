500c340b1e1bd98bc27576f0ed845399b0de5311
timovlad <61474934+timovlad@users.noreply.github.com>
2024-11-13 12:30:40 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -27,10 +27,12 @@ class Vector:
 
     @classmethod
     def create_vector_by_two_points(
-        cls, start_point: Tuple[float, float],
-        end_point: Tuple[float, float]) -> "Vector":
-            return cls(
-            end_point[0] - start_point[0], end_point[1] - start_point[1]
+        cls, start_point: Tuple[float,
+        float], end_point: Tuple[float, float]
+    ) -> "Vector":
+        return cls(
+            end_point[0] - start_point[0],
+            end_point[1] - start_point[1]
         )
 
     def get_length(self) -> float:
