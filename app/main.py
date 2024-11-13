e74186f66ce6b11daf1c1036051a849cf3d2b13a
timovlad <61474934+timovlad@users.noreply.github.com>
2024-11-13 12:05:05 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import math
 from typing import Tuple
 
+
 class Vector:
     def __init__(self, x_coordinate: float, y_coordinate: float) -> None:
         self.x_coordinate = round(x_coordinate, 2)
@@ -21,11 +22,12 @@ class Vector:
             raise ValueError("Unsupported type for multiplication")
 
     @classmethod
-    def create_vector_by_two_points(cls, start_point: Tuple[float, float], end_point: Tuple[float, float]) -> "Vector":
+    def create_vector_by_two_points(cls, start_point: Tuple[float, float], 
+                                    end_point: Tuple[float, float]) -> "Vector":
         return cls(end_point[0] - start_point[0], end_point[1] - start_point[1])
 
     def get_length(self) -> float:
-        return math.sqrt(self.x_coordinate**2 + self.y_coordinate**2)
+        return math.sqrt(self.x_coordinate ** 2 + self.y_coordinate ** 2)
 
     def get_normalized(self) -> "Vector":
         length = self.get_length()
@@ -39,12 +41,4 @@ class Vector:
         if lengths_product == 0:
             return 0
         cos_a = dot_product / lengths_product
-        cos_a = max(min(cos_a, 1), -1)
-        angle = math.degrees(math.acos(cos_a))
-        return round(angle)
-
-    def get_angle(self) -> int:
-        return self.angle_between(Vector(0, 1))
-
-    def rotate(self, degrees: int) -> "Vector":
-        radians = math.radians(degrees)
+        cos_a = max(min(cos_a, 
